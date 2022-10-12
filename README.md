6188e6087b1e1c5924a966ef1b4c5d49a552e68c
somtech <oscar11@gmail.com>
2022-10-12 22:00:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# netflix_login_animation
+
+a netflix login animation using lottie package from flutter
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Lottie.network(
+            'https://assets1.lottiefiles.com/private_files/lf30_is6flrfu.json',
+            repeat: true,
+            frameRate: FrameRate(60),
+            fit: BoxFit.contain),
+      ),
+    );
+  }
+}
